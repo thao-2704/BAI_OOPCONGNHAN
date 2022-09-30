9b3e0c8d0f36cb89501f7e606620c4f0fff9dc3d
thao-2704 <phanthanhthao.2704@gmail.com>
2022-09-30 10:53:06 +0700

    comit2

--- OOP_CONGNHAN.rb
+++ OOP_CONGNHAN.rb
@@ -1,60 +1,78 @@
 # Xay dung lop CanBo
 class CanBo
-    def initialize(name, age, gender, address)
-        @name = name.to_s
-        @age = age.to_i
-        @gender = gender.to_s
-        @address = address.to_s
+    def initialize(name_258, age_258, gender_258, address_258)
+        @name_258 = name_258.to_s
+        @age_258 = age_258.to_i
+        @gender_258_258 = gender_258.to_s
+        @address_258_258 = address_258.to_s
     end
     def display
-        puts "Name: #{@name}"
-        puts "Age: #{@age}"
-        puts "Gender: #{@gender}"
-        puts "Address: #{@address}"
+        puts "Name: #{@name_258_258}"
+        puts "Age: #{@age_258_258}"
+        puts "Gender: #{@gender_258_258}"
+        puts "Address: #{@address_258_258}"
     end
 end
 # Xay dung lop CongNhan
 class Congnhan < CanBo 
-    attr_accessor :level
-    def inputInfCN(level)
-        @level = level.to_i
+    attr_accessor :level_258_258
+    def inputInf(level_258)
+        @level_258_258 = level_258.to_i
     end
 
     def display()
         super
-        puts "Level: #{@level}"
+        puts "Level: #{@level_258_258}"
     end
 end
 # Xay dung lop KySu
 class KySu < CanBo
-    attr_accessor :branch
-        def inputInfKS(branch)
-        @branch = branch.to_s
+    attr_accessor :branch_258_258
+        def inputInf(branch_258)
+        @branch_258 = branch_258.to_s
     end
     def display()
         super
-        puts "Branch: #{@branch}"
+        puts "Branch: #{@branch_258}"
     end
 end
 # Xay dung lop NhanVien
 class NhanVien < CanBo
-    attr_accessor :task
-    def inputInfNV(tash)
-        @task = task.to_s
+    attr_accessor :task_258
+    def inputInf(tash_258)
+        @task_258 = task_258.to_s
     end
     def display()
         super
-        puts "Task: #{@task}"
+        puts "Task: #{@task_258}"
     end
 end
 # Xay dung lop QuanlyCanBo
 
-class QuanlyCanBo()
-    attr_accessor :canboList = []
-    def inputInf()
-        
+class Quanlycanbo
+    $array_offi = Array.new
+    def inputInf(object)
+        $array_offi << object
+    end
+    def outputInfo
+        $array_offi.each do |val|
+            puts "#{val.display}"
+        end
     end
 end
-kysu = KySu.new("Tho","21","Nu","DaNang")
-kysu.inputInf("3")
-kysu.display()
+
+canbo1 = CongNhan.new("Tho","21","Nu","DaNang")
+canbo1.inputInf("1")
+
+
+canbo2 = NhanVien.new("Tho","21","Nu","DaNang")
+canbo2.inputInf("IT")
+
+
+canbo3 = KySu.new("Tho","21","Nu","DaNang")
+canbo3.inputInf("A")
+
+canboList_258 = Quanlycanbo.new
+canboList_258.inputInf(canbo1)
+canboList_258.inputInf(canbo2)
+canboList_258.inputInf(canbo3)
